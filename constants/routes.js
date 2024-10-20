b0316f85db841ceb333c2fccbb6df56cba8c7588
Sebastian Hans Weissenborn <58604870+s2seweis@users.noreply.github.com>
2024-10-20 16:22:34 +0200

    Build Environment Variable

--- constants/routes.js
+++ constants/routes.js
@@ -1,5 +1,6 @@
 // also change in gatsby-config.js
-const apiBase = "http://localhost:3210"
+// const apiBase = "http://localhost:3210"
+const apiBase = process.env.API_BASE
 // const apiBase = "https://hufschuh-app-backend-9bebbc206e13.herokuapp.com"
 // const apiBase = "https://api.hufschuh.app"
 
